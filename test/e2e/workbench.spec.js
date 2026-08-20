a64472eef66e8cebc596b319c0c0d0d1b0c25d64
Nao <dengnao@gmail.com>
2026-08-20 18:08:23 +0800

    ci: make test discovery portable

--- test/e2e/workbench.spec.js
+++ test/e2e/workbench.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('user can create a project from the workbench', async ({ page }) => {
   await page.goto('/');
-  await expect(page.getByRole('heading', { name: /(上午|下午)好/ })).toBeVisible();
+  await expect(page.getByRole('heading', { name: /(上午|下午|晚上)好/ })).toBeVisible();
   await page.getByRole('button', { name: /新建项目/ }).first().click();
   await expect(page.locator('#nc-title')).toBeVisible();
   await page.locator('#nc-title').fill('浏览器回归项目');
